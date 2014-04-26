ff7a4a5cf7a2128fdad2294676bb278cae54c651
Jared Forsyth <jared@jaredforsyth.com>
2014-04-26 01:51:21 -0600

    removing cors stuff

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from flask import Flask, Config, request, jsonify, make_response
-from flask_cors import cross_origin as flask_cors
 import cPickle
 from os.path import join, dirname, exists
 from threading import Timer
@@ -14,9 +13,6 @@ from dao import DAO
 from mempl import MemPL
 
 PICKLE_PATH = join(dirname(__file__), 'dump.pck')
-
-def cross_origin():
-    return flask_cors(headers=['Content-Type', 'Origin', 'X-Requested-With', 'Accept'], origins=['*'], send_wildcard=False)
 
 def make_app(persist=True):
     req = request
@@ -63,19 +59,16 @@ def make_app(persist=True):
     ## Misc
 
     @get('/features')
-    @cross_origin()
     def get_features():
         return jsonify(features=utils.list_features())
 
     @get('/learners')
-    @cross_origin()
     def get_learner():
         return jsonify(learners=utils.list_learners())
 
     ## Projcts
 
     @post('/project/new')
-    @cross_origin()
     def new_project():
         data = req.files['file']
         name = req.form['name']
@@ -83,28 +76,23 @@ def make_app(persist=True):
         return jsonify(id=id, name=name)
 
     @get('/project/')
-    @cross_origin()
     def list_projects():
         return jsonify(names=dao.list_projects())
 
     @get('/project/<int:id>')
-    @cross_origin()
     def get_project(id):
         return jsonify(data=dao.get_project(id))
 
     @post('/project/<int:id>/name')
-    @cross_origin()
     def update_name(id):
         dao.update_name(id, req.json['name'])
         return flask.Response(status=204)
 
     @get('/project/<int:id>/name')
-    @cross_origin()
     def get_name(id):
         return jsonify(name=dao.get_name(id))
 
     @post('/project/<int:id>/data')
-    @cross_origin()
     def update_data(id):
         raw_data, training_data = dao.update_data(id, req.files['file'])
         return jsonify(raw_data=raw_data, training_data=training_data)
@@ -112,12 +100,10 @@ def make_app(persist=True):
     ## Features
 
     @get('/project/<int:id>/feature/all')
-    @cross_origin()
     def feature_output(id):
         return jsonify(**dao.get_feature_output(id))
 
     @post('/project/<int:id>/feature/new')
-    @cross_origin()
     def new_feature(id):
         name = req.json['name']
         type = req.json['type']
@@ -126,25 +112,21 @@ def make_app(persist=True):
         return jsonify(fid=fid, feature_column=feature_column, feature=feature)
 
     @delete('/project/<int:id>/feature/<int:fid>')
-    @cross_origin()
     def remove_feature(id, fid):
         dao.remove_feature(id, fid)
         return flask.Response(status=204)
 
     @post('/project/<int:id>/feature/<int:fid>/name')
-    @cross_origin()
     def update_feature_name(id, fid):
         dao.update_feature_name(id, fid, req.json['name'])
         return flask.Response(status=204)
 
     @post('/project/<int:id>/feature/<int:fid>')
-    @cross_origin()
     def update_feature(id, fid):
         feature_column = dao.update_feature(id, fid, req.json)
         return jsonify(feature_column=feature_column)
 
     @post('/project/<int:id>/feature/<int:fid>/args')
-    @cross_origin()
     def update_feature_args(id, fid):
         feature_column = dao.update_feature_args(id, fid, req.json['args'])
         return jsonify(feature_column=feature_column)
@@ -152,12 +134,10 @@ def make_app(persist=True):
     ## Learners
 
     @get('/project/<int:id>/learner/all')
-    @cross_origin()
     def all_learners(id):
         return jsonify(**dao.get_learner_output(id))
 
     @post('/project/<int:id>/learner/new')
-    @cross_origin()
     def new_learner(id):
         name = req.json['name']
         type = req.json['type']
@@ -166,7 +146,6 @@ def make_app(persist=True):
         return jsonify(lid=lid, accuracy=acc, confusion=confusion, learner=learner, classes=classes, assignments=assignments)
 
     @post('/project/<int:id>/learner/<int:lid>')
-    @cross_origin()
     def update_learner(id, lid):
         name = req.json.get('name', None)
         args = req.json.get('args', None)
@@ -174,13 +153,11 @@ def make_app(persist=True):
         return flask.Response(status=204)
 
     @delete('/project/<int:id>/learner/<int:fid>')
-    @cross_origin()
     def remove_learner(id, fid):
         dao.remove_learner(id, fid)
         return flask.Response(status=204)
 
     @post('/project/<int:id>/train/all')
-    @cross_origin()
     def train_all(id):
         start = time.time()
         confusions = dao.train_all(id)
@@ -188,7 +165,6 @@ def make_app(persist=True):
         return jsonify(time=dur, confusions=confusions)
 
     @post('/project/<int:id>/train/<int:lid>')
-    @cross_origin()
     def train_one(id, lid):
         start = time.time()
         confusion = dao.train(id, lid)
@@ -197,7 +173,6 @@ def make_app(persist=True):
 
     '''
     @get('/project/<int:id>/compiled/<int:lid>/<target>')
-    @cross_origin()
     def get_compiled(id, target, lid):
         if not dao.has_target(target):
             return flask.Response('Cannot compile to that target', status=404)
@@ -208,7 +183,6 @@ def make_app(persist=True):
     '''
 
     @get('/project/<int:id>/img/<int:instid>')
-    @cross_origin()
     def get_img(id, instid):
         data = dao.get_img(id, instid)
         if not data:
@@ -218,7 +192,6 @@ def make_app(persist=True):
         return res
 
     @get('/project/<int:id>/vid/<int:instid>')
-    @cross_origin()
     def get_vid(id, instid):
         data = dao.get_vid(id, instid)
         if not data:
@@ -228,7 +201,6 @@ def make_app(persist=True):
         return res
 
     @get('/project/<int:id>/compiled/<int:lid>')
-    @cross_origin()
     def get_compiled(id, lid):
         features, trained, learner = dao.trained_learner(id, lid)
         zipfile = package.learner(features, trained, learner)
